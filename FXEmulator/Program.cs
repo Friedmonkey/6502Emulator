3d6a2e2a9e52c54f1521213b453f5cc69d6abec1
Friedmonkey <Martijn.kraats5@gmail.com>
2023-09-24 15:34:14 +0200

    changed the code to render a triangole thingy to test if most op codes work, which they all seem to do :D

--- FXEmulator/Program.cs
+++ FXEmulator/Program.cs
@@ -13,29 +13,76 @@ public static class Program
 		emu.data[0xfffc] = 0x00;
 		emu.data[0xfffd] = 0x06;
 
+        //     emu.data.AddCodeAt(0x0600,
+        //0xa2, 0x00,			//	LDX #$00
+        //0xa9, 0x01,			//	LDA #$01
+        //					//LOOP:
+        //0x9d, 0x00, 0x02,	//	STA $0200, X
+        //0x9d, 0x00, 0x03,	//	STA $0300, X
+        //0x9d, 0x00, 0x04,	//	STA $0400, X
+        //0x9d, 0x00, 0x05,	//	STA $0500, X
+        //0xe8,               //	INX
+        //         0x69, 0x00,			//	ADC #$00
+
+
+        //         0xe0, 0x00,			//	CPX #$00
+        //0xd0, 0xed,			//	BNE LOOP
+
+        //0xa9, 0x02,			//	LDA #$02
+        //0x8d, 0x50, 0x03,   //	STA $0350
+
+        //         0xa9, 0x03,         //	LDA #$03
+        //         0x8d, 0x51, 0x03,	//	STA $0352
+
+        //         0x00				//	BRK
+        //     );
+
         emu.data.AddCodeAt(0x0600,
-			0xa2, 0x00,			//	LDX #$00
-			0xa9, 0x01,			//	LDA #$01
-								//LOOP:
-			0x9d, 0x00, 0x02,	//	STA $0200, X
-			0x9d, 0x00, 0x03,	//	STA $0300, X
-			0x9d, 0x00, 0x04,	//	STA $0400, X
-			0x9d, 0x00, 0x05,	//	STA $0500, X
-			0xe8,               //	INX
-            0x69, 0x00,			//	ADC #$00
-
-
-            0xe0, 0x00,			//	CPX #$00
-			0xd0, 0xed,			//	BNE LOOP
-
-			0xa9, 0x02,			//	LDA #$02
-			0x8d, 0x50, 0x03,   //	STA $0350
-
-            0xa9, 0x03,         //	LDA #$03
-            0x8d, 0x51, 0x03,	//	STA $0352
-
-            0x00				//	BRK
-        );
+                                        // HIGHLIGHTSTART:      0x0600
+            0xA2, 0x00,                 //        LDX #$00
+            0xA9, 0x00,                 //        LDA #$00
+            0x85, 0x00,                 //        STA $00
+            0xA9, 0x02,                 //        LDA #$02
+            0x85, 0x01,                 //        STA $01
+                                        // WRITE:               0x060A
+            0x20, 0x1F, 0x06,           //        JSR $061F
+            0x81, 0x00,                 //        STA ($00,X)
+            0xE6, 0x00,                 //        INC $00
+            0xF0, 0x03,                 //        BEQ $0016
+            0x4C, 0x0A, 0x06,           //        JMP $006A
+                                        // HIGHBYTE:            0x0616
+            0xE6, 0x01,                 //        INC $01
+            0xA4, 0x01,                 //        LDY $01
+            0xC0, 0x06,                 //        CPY #$06
+            0xD0, 0xEC,                 //        BNE $000A
+            0x60,                       //        RTS
+                                        // POINT:               0x061F
+            0xA5, 0x00,                 //        LDA $00
+            0x29, 0x1F,                 //        AND #$1F
+            0x85, 0x02,                 //        STA $02 ;x
+            0xA5, 0x00,                 //        LDA $00
+            0x4A,                       //        LSR A
+            0x4A,                       //        LSR A
+            0x4A,                       //        LSR A
+            0x4A,                       //        LSR A
+            0x4A,                       //        LSR A
+            0x85, 0x03,                 //        STA $03
+            0xA5, 0x01,                 //        LDA $01
+            0x38,                       //        SEC
+            0xE9, 0x02,                 //        SBC #$02
+            0x0A,                       //        ASL A
+            0x0A,                       //        ASL A
+            0x0A,                       //        ASL A
+            0x05, 0x03,                 //        ORA $03
+            0x25, 0x02,                 //        AND $02
+            0xF0, 0x03,                 //        BEQ $003F
+            0xA9, 0x02,                 //        LDA #$02
+            0x60,                       //        RTS
+                                        // OKAY:                0x063F
+            0xA9, 0x0D,                 //        LDA #$0D
+            0x60                        //        RTS
+            
+            );
 
         emu.Debug = false;
 		emu.reset();
@@ -58,7 +105,7 @@ public static class Program
 
         while (!emu.CheckFlag(sf.I))
 		{
-			emu.step();
+            emu.step();
 		}
 	}
 
